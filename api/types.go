ea3c47ab4040340603b6097a6f5cac29cbf4818b
Phillip <phillip@freitagsrunde.org>
2018-10-30 22:22:40 +0100

    Modified channel model for im support

--- api/types.go
+++ api/types.go
@@ -1,18 +1,19 @@
 package api
 
 type Channel struct {
-	Id           string `json:"_id"`
-	Name         string `json:"name"`
-	MessageCount int `json:"msgs"`
+	Id           string   `json:"_id"`
+	Name         string   `json:"name"`
+	MessageCount int      `json:"msgs"`
 	UserNames    []string `json:"usernames"`
+	UserName     string   `json:"username"`
 
-	User         User `json:"u"`
+	User User `json:"u"`
 
-	ReadOnly     bool `json:"ro"`
-	Timestamp    string `json:"ts"`
-	T            string `json:"t"`
-	UpdatedAt    string `json:"_updatedAt"`
-	SysMes       bool `json:"sysMes"`
+	ReadOnly  bool   `json:"ro"`
+	Timestamp string `json:"ts"`
+	T         string `json:"t"`
+	UpdatedAt string `json:"_updatedAt"`
+	SysMes    bool   `json:"sysMes"`
 }
 
 type User struct {
@@ -31,36 +32,36 @@ type Message struct {
 	ChannelId string `json:"rid"`
 	Text      string `json:"msg"`
 	Timestamp string `json:"ts"`
-	User      User `json:"u"`
+	User      User   `json:"u"`
 }
 
 type Info struct {
 	Version string `json:"version"`
 
 	Build struct {
-		Date string `json:"date"`
+		Date        string `json:"date"`
 		NodeVersion string `json:"nodeVersion"`
-		Arch string `json:"arch"`
-		Platform string `json:"platform"`
-		OsRelease string `json:"osRelease"`
-		TotalMemory int64 `json:"totalMemory"`
-		FreeMemory int64 `json:"freeMemory"`
-		CpuCount int `json:"cpus"`
+		Arch        string `json:"arch"`
+		Platform    string `json:"platform"`
+		OsRelease   string `json:"osRelease"`
+		TotalMemory int64  `json:"totalMemory"`
+		FreeMemory  int64  `json:"freeMemory"`
+		CpuCount    int    `json:"cpus"`
 	} `json:"build"`
 
 	Travis struct {
 		BuildNumber string `json:"buildNumber"`
-		Branch string `json:"branch"`
-		Tag string `json:"tag"`
+		Branch      string `json:"branch"`
+		Tag         string `json:"tag"`
 	} `json:"travis"`
 
-	Commit  struct {
-		Hash string `json:"hash"`
-		Date string `json:"date"`
-		Author string `json:"author"`
+	Commit struct {
+		Hash    string `json:"hash"`
+		Date    string `json:"date"`
+		Author  string `json:"author"`
 		Subject string `json:"subject"`
-		Tag string `json:"tag"`
-		Branch string `json:"branch"`
+		Tag     string `json:"tag"`
+		Branch  string `json:"branch"`
 	} `json:"commit"`
 
 	GraphicsMagick struct {
@@ -68,7 +69,7 @@ type Info struct {
 	} `json:"GraphicsMagick"`
 
 	ImageMagick struct {
-		Enabled bool `json:"enabled"`
+		Enabled bool   `json:"enabled"`
 		Version string `json:"version"`
 	} `json:"ImageMagick"`
 }
